5aa27312b9b4b7d2bb0701199f49215808a8fced
Sanket <sanketchopade6@gmail.com>
2026-01-02 15:23:28 +0530

    update tf files

--- Terraform_EKS_pipeline/main.tf
+++ Terraform_EKS_pipeline/main.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_version = ">= 1.3.0"
+# terraform {
+#   required_version = ">= 1.3.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
-    }
-  }
-}
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "~> 2.25"
+#     }
+#   }
+# }
 
 ############################
 # AWS Provider
